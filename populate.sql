6e479b41e79c1257c14218115694434e488a1281
Gabriel Augusto Rocco <up201800172@fe.up.pt>
2022-10-31 23:41:42 +0000

    Upload New File

---
+++ populate.sql
@@ -0,0 +1,142 @@
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(1, 'gife0@census.gov', 'Gabbie Ife', 'Female', 'SvHEEV', FALSE, 'enable impactful deliverables', 'https://robohash.org/solutacumqueet.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(2, 'dbeartup1@ucoz.com', 'Denis Beartup', 'Male', '81ebfyZF', FALSE, 'mesh out-of-the-box relationships', 'https://robohash.org/sintbeataefugiat.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(3, 'rablitt2@rambler.ru', 'Rockie Ablitt', 'Male', 'FTYw5vBL0Y', FALSE, 'empower global e-commerce', 'https://robohash.org/architectodoloremnon.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(4, 'wpinckstone3@ox.ac.uk', 'Wesley Pinckstone', 'Female', 'ZJhADAvY', FALSE, 'repurpose enterprise functionalities', 'https://robohash.org/nisipraesentiumqui.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(5, 'mmonsey4@friendfeed.com', 'Marsh Monsey', 'Male', 'ki8Zu9yWD7', FALSE, 'synergize distributed niches', 'https://robohash.org/sedassumendadebitis.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(6, 'bhouchin5@uol.com.br', 'Brewster Houchin', 'Female', 'gT6qvtpNw5H', FALSE, 'maximize visionary schemas', 'https://robohash.org/etquiid.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(7, 'aroostan6@columbia.edu', 'Augie Roostan', 'Female', 'U7EXzGdI42I', FALSE, 'redefine extensible supply-chains', 'https://robohash.org/quiaetinventore.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(8, 'sfawkes7@gnu.org', 'Sonny Fawkes', 'Male', 'SBT9NW', FALSE, 'strategize value-added partnerships', 'https://robohash.org/doloremexvoluptatem.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(9, 'jsimpkiss8@networkadvertising.org', 'Josefina Simpkiss', 'Male', 'ZfKGY5iOsheM', FALSE, 'enable impactful experiences', 'https://robohash.org/pariaturofficiaquia.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(10, 'hdulwitch9@slideshare.net', 'Hilary Dulwitch', 'Male', 'oXr4xIC', FALSE, 'utilize front-end systems', 'https://robohash.org/aliasvoluptateseveniet.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(11, 'sventurolia@guardian.co.uk', 'Shanan Venturoli', 'Female', 'Z5UnQ7', FALSE, 'mesh impactful web services', 'https://robohash.org/perferendisremesse.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(12, 'nkoubekb@vk.com', 'Norbert Koubek', 'Male', 'eZZC3B', FALSE, 'optimize 24/7 users', 'https://robohash.org/corporisidiure.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(13, 'astoppec@blog.com', 'Alla Stoppe', 'Female', '9S4CxRvz', FALSE, 'integrate visionary solutions', 'https://robohash.org/optiovelitvoluptatum.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(14, 'sbockd@creativecommons.org', 'Silvan Bock', 'Male', 'aNkPmKGq8n2', FALSE, 'disintermediate e-business interfaces', 'https://robohash.org/temporaplaceatnihil.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(15, 'twillartone@pen.io', 'Tabbatha Willarton', 'Male', 'TkYtau', FALSE, 'cultivate open-source paradigms', 'https://robohash.org/voluptasnequenisi.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(16, 'abollamf@gizmodo.com', 'Alaster Bollam', 'Female', 'jBhiYwcU', FALSE, 'engage sticky vortals', 'https://robohash.org/dolorvoluptatibusesse.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(17, 'hpringellg@smh.com.au', 'Hamil Pringell', 'Male', 'UteLB5bt', FALSE, 'harness bleeding-edge markets', 'https://robohash.org/iustoesseconsequuntur.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(18, 'gbrendekeh@guardian.co.uk', 'Garrek Brendeke', 'Male', 'gyrXnPstE', FALSE, 'evolve seamless architectures', 'https://robohash.org/idsuntipsa.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(19, 'kgailori@oakley.com', 'Kizzie Gailor', 'Male', 'IGUZjWx970', FALSE, 'unleash cross-media infomediaries', 'https://robohash.org/estmaximeoptio.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(20, 'rodownej@constantcontact.com', 'Ruy O''Downe', 'Male', 'WdksP42J7', FALSE, 'reinvent impactful functionalities', 'https://robohash.org/minimamolestiaelaudantium.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(21, 'cgodonk@sciencedirect.com', 'Celina Godon', 'Female', 'QQN0D3rWJk', FALSE, 'redefine dot-com web-readiness', 'https://robohash.org/maioreslaudantiumdignissimos.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(22, 'rquittondenl@etsy.com', 'Rona Quittonden', 'Female', '9TJ4V010n', FALSE, 'generate extensible relationships', 'https://robohash.org/totamvitaeaut.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(23, 'cludronm@elegantthemes.com', 'Cathy Ludron', 'Male', 'pDyV9328gF', FALSE, 'synergize world-class eyeballs', 'https://robohash.org/distinctioconsequaturtempora.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(24, 'ascoggansn@discuz.net', 'Andrej Scoggans', 'Male', 'Fs7ZQczoSCT', FALSE, 'deploy robust infomediaries', 'https://robohash.org/abeaquevoluptas.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(25, 'tbroughtono@merriam-webster.com', 'Trixy Broughton', 'Female', 'gcTeoC7bB', FALSE, 'leverage cutting-edge platforms', 'https://robohash.org/quiadbeatae.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(26, 'scalbertp@pen.io', 'Shalna Calbert', 'Female', '3ZgHNwS65ZYy', FALSE, 'target leading-edge bandwidth', 'https://robohash.org/autfugaiusto.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(27, 'odearlepalserq@rediff.com', 'Orin Dearle-Palser', 'Female', '2uHiSXQMBx3', FALSE, 'seize open-source e-tailers', 'https://robohash.org/dignissimosoptioofficia.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(28, 'bemblinr@bizjournals.com', 'Brittaney Emblin', 'Male', 'QZBJ12', TRUE, 'strategize visionary e-markets', 'https://robohash.org/natusplaceatmolestiae.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(29, 'aphebeys@umn.edu', 'Adina Phebey', 'Female', 'Nyc8H7Xs9qM', TRUE, 'e-enable intuitive relationships', 'https://robohash.org/etquiet.png?size=50x50&set=set1');
+INSERT INTO Users (id, email, name, gender, password, ban, description, photo) VALUES(30, 'daslumt@dion.ne.jp', 'Darci Aslum', 'Male', 'RPX1pG', TRUE, 'transition 24/365 schemas', 'https://robohash.org/aperiamnumquammaiores.png?size=50x50&set=set1');
+
+
+
+
+
+
+INSERT INTO Post(author, text, photo, visibility) VALUES(15,'10 Python Mini Automation Projects','https://pbs.twimg.com/profile_images/429285908953579520/InZKng9-_x96.jpeg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(4,'The 3 best ADVANCED techniques I learned while building in public last month were:,,1) How to use Chrome Inspect Console properly, especially the Network - Fetch/XHR - Responses feature.,,Understanding the requests from a site is life changing for web scraping.,,(1/3)','https://pbs.twimg.com/profile_images/1461710621397897223/XZciUUbZ_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(18,'Halloween is coming','https://pbs.twimg.com/profile_images/1269253433703510016/B6XjoBkv_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(6,'October traffic has been in the red so far.,,Heres how I plan to make it green: Create helpful and relevant posts in communities (Reddit, Facebook, IH),, Run an engineering as marketing campaign. Experiment with content marketing. As usual, will share everything here ','https://pbs.twimg.com/profile_images/1535551343036964865/13nIczOn_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(5,'Puppy thinks hes a bunny.. ','https://pbs.twimg.com/profile_images/1130022182971760640/FlbICzEn_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(5,'The Psychology of Manipulation You Should Know Before Its Too Late...','https://pbs.twimg.com/profile_images/1431998771349626882/614uNv3Y_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(24,' Hacked filter: sepia() to colorize white walls by masking them with a canvas from an image from auto detected segment that adds it as a image-mask to the image ,,So that you user can soon colorize any walls with a slider','https://pbs.twimg.com/profile_images/1562107516066095106/IUccJ78Y_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(5,'-you*','https://pbs.twimg.com/profile_images/1562107516066095106/IUccJ78Y_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(16,'20 must-watch movies that will change your life & mindset:,,1. Schindler’s List','https://pbs.twimg.com/profile_images/1568369300909473793/JUHo6Ali_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(7,'Let us not forget the Indian version of Michael Jacksons THRILLER!','https://pbs.twimg.com/profile_images/1243734573764071424/wTvOVSyJ_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(4,'Heading for a $5k+ month ,,Someone once told me I`d never make it into the 40% tax bracket and yet here we are. A big thanks to all my clients, sponsors and supporters','https://abs-0.twimg.com/emoji/v2/svg/26a1.svg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(25,'Congratulations to Ulf Kristersson ,@moderaterna, on being elected PM of Sweden. Im sure - cooperation will continue active development, and ’s support in the fight against Russian aggression will increase. I am grateful to Magdalena Andersson for solidarity & supporting .','https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(21,'Steps to become a data analyst (my preference) :,,Stats (yes),↓,,SQL,↓,,Storytelling with data,(Tableau/ PowerBI),↓,,Build dashboards,↓,,Python,↓,,Visualization libraries,(Pandas, Matplotlib, NumPy, Seaborn),Portfolio projects,(From data cleaning → Data analysis)','https://pbs.twimg.com/profile_images/1581653962822152193/YxHE5AVT_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(14,'En route to SF for TechCrunch Disrupt. ,,Seems like we have a fun crew here already. Who else should I meet?,,And who wants to join our tech crew?','https://abs-0.twimg.com/emoji/v2/svg/1f499.svg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(7,'A historic failure','https://pbs.twimg.com/profile_images/1085150433960706048/_T5T_iZY_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(5,'Trabalhe mais feliz com os portáteis HP com a plataforma Intel® Evo™. Com a funcionalidade HP de redução do ruído pode trabalhar de onde quiser, quando quiser.','https://pbs.twimg.com/profile_images/1542882499851845635/wwafo2gn_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(24,'Uncanny ','https://pbs.twimg.com/profile_images/1210913904597110784/8tFdcHPs_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(5,'Mountain Climber Fights Off Bear (2022)','https://pbs.twimg.com/profile_images/1495825338806554631/9g1Bm6qt_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(17,'Playful bear seems to dance in the forest','https://pbs.twimg.com/profile_images/557958031606939649/jExOWkdO_x96.jpeg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(9,'Lose your belly fat','https://pbs.twimg.com/profile_images/1581205234407247872/eeoMBwIb_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(19,'Submitting Tailscan to the Chrome Web Store later today! Yesterday, I finished one of the last core features:,,Tailwind CSS config support!,,You can save your own config so that all your custom classes and variants are available','https://pbs.twimg.com/profile_images/1574976813272813570/qASrP2yS_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(3,'mhuaw','https://pbs.twimg.com/profile_images/1581003527735894017/Kwb-vg5A_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(24,'If you want to build an African startup, its not about copying the Silicon Valley model.,,Its about finding the *African* way to do things. Different problems, different contexts, different solutions.','https://pbs.twimg.com/profile_images/1480420844941848580/eiJBfe6h_x96.jpg','Friends');
+INSERT INTO Post(author, text, photo, visibility) VALUES(23,'Do you work in English? Check our specialized search engine for English-speaking professionals looking for opportunities in Portugal.','https://pbs.twimg.com/profile_images/481545308094996480/pP7T4KkS_x96.png','Friends');
+
+
+
+
+
+
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 2, 'Close Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 3, 'Close Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 4, 'Close Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 5, 'Close Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 6, 'Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 7, 'Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(2, 7, 'Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(2, 8, 'Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(2, 9, 'Family');
+INSERT INTO Relationship(user1, user2, type) VALUES(2, 10, 'Family');
+INSERT INTO Relationship(user1, user2, type) VALUES(2, 11, 'Family');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 12, 'Family');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 13, 'Family');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 14, 'Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 15, 'Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 16, 'Family');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 17, 'Family');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 18, 'Friends');
+INSERT INTO Relationship(user1, user2, type) VALUES(1, 19, 'Close Friends');
+
+
+
+INSERT INTO CommentPost(author, post, text) VALUES(1, 1, 'Thank you for the information');
+INSERT INTO CommentPost(author, post, text) VALUES(2, 9, 'I will try it');
+INSERT INTO CommentPost(author, post, text) VALUES(3, 5, 'Thats really nice!!!');
+INSERT INTO CommentPost(author, post, text) VALUES(4, 1, 'I will try it it soon, thank you');
+INSERT INTO CommentPost(author, post, text) VALUES(4, 4, 'I dont know if it will work');
+INSERT INTO CommentPost(author, post, text) VALUES(5, 1, 'I think that building a website is not that hard');
+INSERT INTO CommentPost(author, post, text) VALUES(5, 2, 'Well deserved. You write good content. Glad to have you here');
+INSERT INTO CommentPost(author, post, text) VALUES(6, 4, 'Cheers to more good reads.');
+INSERT INTO CommentPost(author, post, text) VALUES(6, 1, 'I use Linkedin mainly to offer and find new job opportunities.');
+INSERT INTO CommentPost(author, post, text) VALUES(19, 1, 'I wish you the best for 2022!!!');
+INSERT INTO CommentPost(author, post, text) VALUES(13, 7, 'I am not a master. but I like Master of Puppets and I have been a D&D Dungeon Master. does it count?');
+INSERT INTO CommentPost(author, post, text) VALUES(14, 7, 'Wishing you more success and great year');
+
+
+INSERT INTO CommentReply(author, comment, text) VALUES(7, 1, 'You are welcome :)');
+INSERT INTO CommentReply(author, comment, text) VALUES(8, 1, 'Cheers to more good reads.');
+INSERT INTO CommentReply(author, comment, text) VALUES(9, 5, 'I am glad you liked it');
+INSERT INTO CommentReply(author, comment, text) VALUES(10, 1, 'Looking forward to learn more from you in 2022.');
+INSERT INTO CommentReply(author, comment, text) VALUES(16, 6, 'Good luck on your journey Captain!');
+INSERT INTO CommentReply(author, comment, text) VALUES(11, 6, 'Will always follow you');
+INSERT INTO CommentReply(author, comment, text) VALUES(12, 8, 'You are definitely a great person, already following you.');
+INSERT INTO CommentReply(author, comment, text) VALUES(18, 9, 'Have just downloaded Docker to complete @freeCodeCamp Relational Database Cert using Docker and VSCode... wish me luck!');
+
+
+-- random Post reactions INSERT
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 1, 'Amazed');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 2, 'Dislike');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 3, 'Sad');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 4, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 5, 'Dislike');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 6, 'Amazed');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 7, 'Amazed');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 8, 'Dislike');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 9, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 10, 'Sad');
+INSERT INTO PostReaction(post, userId, type) VALUES(1, 11, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(2, 12, 'Amazed');
+INSERT INTO PostReaction(post, userId, type) VALUES(2, 13, 'Dislike');
+INSERT INTO PostReaction(post, userId, type) VALUES(2, 14, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(2, 15, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(2, 16, 'Amazed');
+INSERT INTO PostReaction(post, userId, type) VALUES(2, 17, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(2, 18, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(4, 19, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(5, 13, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(6, 12, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(8, 19, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(9, 11, 'Like');
+INSERT INTO PostReaction(post, userId, type) VALUES(3, 19, 'Like');
+
+
+INSERT INTO CommentPost(author, post, text) VALUES(1, 1, 'Thank you for the information');
+INSERT INTO CommentPostReaction(comment, userId, type) VALUES(1, 15, 'Like');
+
