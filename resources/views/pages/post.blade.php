8c1b3a9069d3694d0055cc6bc670aa0cf884e42f
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2022-11-16 12:05:41 +0000

    name of post author

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <article class="post" data-id="{{ $post->id }}">
 <header>
-  <h2>{{ $post->id }}</a></h2>
+  <h2>{{ $post->user()->get()[0]->name }}</a></h2>
 </header>
 <div>{{ $post->text }}</div>
 <img src={{ $post->photo }} > 
--- resources/views/partials/post.blade.php
+++ resources/views/partials/post.blade.php
@@ -1,6 +1,6 @@
 <article class="post" data-id="{{ $post->id }}">
 <header>
-    <h2><a href="/posts/{{ $post->id }}">{{ $post->id }}</a></h2>
+    <h2><a href="/posts/{{ $post->id }}">{{ $post->user()->get()[0]->name }}</a></h2>
 </header>
 <div>{{ $post->text }}</div>
 </article>
