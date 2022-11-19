b4970d135cde1d2787d54a973ea23a8e5fd1eca8
Guilherme Cunha Seco Fernandes de Almeida <up202008866@fe.up.pt>
2022-11-19 23:42:56 +0000

    Feature/posts actions

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -4,6 +4,22 @@
 
 @section('content')
 
+<section id="new_post">
+  <article style="border-style: solid;color: Tomato">
+    <header>
+      <h2 onclick="location.href='/user/{{Auth::user()->id}}';" style="cursor:pointer;" ><img src={{ Auth::user()->photo }}</a></h2>
+    </header>
+    <?php
+      echo Form::open(array('url' => 'api/posts', 'method' => 'put'));
+      echo Form::textarea('text', 'Share your thoughts with us...');
+      echo "Visibility";
+      echo Form::select('visibility', array('Close Friends' => 'Close Friends', 'Friends' => 'Friends', 'Family' => 'Family', 'Strangers' => 'Strangers'));
+      echo Form::button('Create new Post', array('type' => 'submit'));
+      echo Form::close();
+    ?>
+    </article>
+</section>
+
 <section id="posts">
   @each('partials.post', $posts, 'post')
 </section>
