036e58917f4b19972006d32f914e2e69cc9e8662
Gabriel Rocco <gabriel.rocco@icloud.com>
2022-11-21 05:29:28 +0000

    adding templates

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -14,7 +14,7 @@
             <div>{{ $user->description }}</div>
 
             @if ($user->id == Auth::user()->id)
-                <button onclick="toggleEditProfilePopUp()">Edit Profile</button>
+                <button class="btn-primary btn" onclick="toggleEditProfilePopUp()">Edit Profile</button>
                 <div class="popup" id="popup2">
                     <div class='overlay'></div>
                     <div class='content'>
@@ -28,7 +28,7 @@
                         echo Form::textarea('gender', $user->gender);
                         echo 'Description';
                         echo Form::textarea('description', $user->description);
-                        echo Form::button('Edit Profile', ['type' => 'submit']);
+                        echo Form::button('Edit Profile', ['type' => 'submit'], ['class' => 'btn-primary btn']);
                         echo Form::close();
                         ?>
                     </div>
@@ -36,13 +36,15 @@
 
                 <?php
                 echo Form::open(['url' => 'api/user/' . $user->id, 'method' => 'delete']);
-                echo Form::button('Delete Account', ['type' => 'submit']);
+                echo Form::button('Delete Account', ['type' => 'submit'], ['class' => 'btn-danger btn']);
                 echo Form::close();
                 ?>
             @endif
 
             <section id="relationships">
-                <h3 style="color: Blue">RELATIONSHIPS</h2>
+                <h2 style="font-size:19px; font-weight:bold;margin-top:30px;">RELATIONSHIPS</h2>
+                <hr>
+
                     @each('partials.relationship', $user->relationships()->get(), 'relationship')
                     @each('partials.relationship', $user->relationships2()->get(), 'relationship')
             </section>
--- resources/views/partials/post.blade.php
+++ resources/views/partials/post.blade.php
@@ -1,17 +1,44 @@
-<article style="border-style: solid;color: Tomato">
-<header>
-    <h3 onclick="location.href='/user/{{ $post->user()->get()[0]->id }}'" style="color:DodgerBlue;cursor:pointer">{{ $post->user()->get()[0]->getName() }}<img src={{ $post->user()->get()[0]->photo }}</h2>
-</header>
-<div onclick="location.href='/posts/{{ $post->id }}';" class="post" data-id="{{ $post->id }}" style="border-style: solid;cursor: pointer;color: Green;margin:1em">
-    {{ $post->text }}
-</div>
-<div>Number of comments:{{$post->comments()->count()}} 
-</div>
-<div>Number of reactions:{{$post->reactions()->count()}} 
-Like:{{$post->reactions()->where('type','Like')->count()}}
-Dislike:{{$post->reactions()->where('type','Dislike')->count()}}
-Sad:{{$post->reactions()->where('type','Sad')->count()}}
-Angry:{{$post->reactions()->where('type','Angry')->count()}}
-Amazed:{{$post->reactions()->where('type','Amazed')->count()}}
-</div>
-</article>
+<div class="post card" data-id="{{ $post->id }}">
+          <div class="post-header">
+              <img src={{ $post->user()->get()[0]->photo }} class="post-profile-pic">
+              <div class="post-header-info">
+                  <h3><a href="/user/{{ $post->user()->get()[0]->id }}">{{ $post->user()->get()[0]->getName() }}</a></h3>
+                  <p>1 hour ago</p>
+              </div>
+          </div>
+          <a class="post-body" href="/posts/{{ $post->id }}">
+              <p>{{ $post->text }}</p>
+              <!-- <img src="https://www.algavalor.pt/wp-content/uploads/2020/04/FEUP-Autoria-Global-Imagens.jpg" class="post-image"> -->
+          </a>
+          <div class="post-footer">
+              <span class="comment-label">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-message-circle-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.75" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1"></path>
+                  </svg>
+                  <span>{{$post->comments()->count()}}</span>
+              </span>
+              <div class="reaction-holder">
+                  <span class="reaction-label">
+                      <span>👍🏻</span>
+                      <span>{{$post->reactions()->where('type','Like')->count()}}</span>
+                  </span>
+                  <span class="reaction-label">
+                      <span>👎🏻</span>
+                      <span>{{$post->reactions()->where('type','Dislike')->count()}}</span>
+                  </span>
+                  <span class="reaction-label">
+                      <span>😿</span>
+                      <span>{{$post->reactions()->where('type','Sad')->count()}}</span>
+                  </span>
+                  <span class="reaction-label">
+                      <span>😡</span>
+                      <span>{{$post->reactions()->where('type','Angry')->count()}}</span>
+                  </span>
+                  <span class="reaction-label">
+                      <span>😍</span>
+                      <span>{{$post->reactions()->where('type','Amazed')->count()}}</span>
+                  </span>
+              </div>
+          </div>
+      </div>
